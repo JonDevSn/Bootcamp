c3211ccd09342c0f615556e1b0d200c51ca11af2
Devansh Kumar <devansh.kumar.cs27@iilm.edu>
2026-08-10 09:25:19 +0530

    Add Day 3 and Day 4 solutions and log update

---
+++ Day-3/Classwork/LC152.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        curr_max = nums[0]
+        curr_min = nums[0]
+        answer = nums[0]
+
+        for i in range(1, len(nums)):
+            num = nums[i]
+
+            # Negative number swaps max and min
+            if num < 0:
+                curr_max, curr_min = curr_min, curr_max
+
+            curr_max = max(num, curr_max * num)
+            curr_min = min(num, curr_min * num)
+
+            answer = max(answer, curr_max)
+
+        return answer
